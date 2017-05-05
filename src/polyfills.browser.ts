68cf5296d48d4a531bfc1ef4c215109fca675380
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-05-05 07:40:56 +0300

    finished project initialization

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -1,25 +1,43 @@
-/*
- * Angular bootstrapping
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { decorateModuleRef } from './app/environment';
-import { bootloader } from '@angularclass/hmr';
-/*
- * App Module
- * our top level module that holds all of our components
- */
-import { AppModule } from './app';
+// TODO(gdi2290): switch to DLLs
 
-/*
- * Bootstrap our Angular app with a top level NgModule
- */
-export function main(): Promise<any> {
-  return platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .then(decorateModuleRef)
-    .catch((err) => console.error(err));
+// Polyfills
+
+// import 'ie-shim'; // Internet Explorer 9 support
+
+// import 'core-js/es6';
+// Added parts of es6 which are necessary for your project or your browser support requirements.
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/weak-set';
+import 'core-js/es6/typed';
+import 'core-js/es6/reflect';
+// see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
+// import 'core-js/es6/promise';
+
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+
+if ('production' === ENV) {
+  // Production
+
+} else {
+
+  // Development
+  Error.stackTraceLimit = Infinity;
+
+  /* tslint:disable no-var-requires */
+  require('zone.js/dist/long-stack-trace-zone');
+
 }
-
-// needed for hmr
-// in prod this is replace for document ready
-bootloader(main);
